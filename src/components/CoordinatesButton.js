f73c3396287a162d8ecfff9be9bb13df4e737f19
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2021-01-23 19:09:00 +0000

    Automatically backed up by Learn

--- src/components/CoordinatesButton.js
+++ src/components/CoordinatesButton.js
@@ -1,4 +1,20 @@
 // Code CoordinatesButton Component Here
 import React, {component} from 'react'
 
-export default class CoordinatesButton extends 
+export default class CoordinatesButton extends React.Component {
+ constructor(){
+   super()
+   
+   
+ }
+ 
+ render(){
+   return (
+     <div>
+     
+     </div>
+     )
+ }
+ 
+  
+}

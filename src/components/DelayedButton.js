f93eaae7c50f97ea61aa95a94dafd08e7117ad8f
Maryam Abdel-karim <maryam.abdelkarim@hotmail.com>
2021-01-23 19:09:27 +0000

    Automatically backed up by Learn

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,19 @@
 // Code DelayedButton Component Here
+import React, {component} from 'react'
+
+export default class DelayedButton extends React.Component {
+ constructor(){
+   super()
+   
+   
+ }
+ 
+ render(){
+   return (
+     <div>
+     
+     </div>
+     )
+ }
+ 
+  
